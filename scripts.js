4263739d5cf56a591762d6e2e136314b80224b15
Pugamodi <104040098+Pugamodi@users.noreply.github.com>
2024-09-16 06:08:06 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita el envío del formulario para demostración
     
-    alert('Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.');
+    alert('Gracias por tu mensaje. Me pondré en contacto contigo pronto.');
     
     // Aquí podrías agregar código para enviar el formulario a un servidor
 });
